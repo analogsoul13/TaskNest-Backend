d8d38428a5c6b82bb139a78d7634a3cb46ce5f74
analogsoul13 <analogsoul13@gmail.com>
2025-04-03 17:11:18 +0530

    Job Filters and pagination Added

--- Controller/jobController.js
+++ Controller/jobController.js
@@ -26,8 +26,51 @@ exports.createJob = async (req, res) => {
 
 exports.getJobs = async (req, res) => {
     try {
-        const jobs = await Job.find().populate("postedBy", "name email")
-        res.status(200).json(jobs)
+
+        const { search, category, minBudget, maxBudget, page, limit } = req.query
+        let query = {}
+
+        // Search
+        if (search) {
+            query.title = { $regex: search, $options: "i" }
+        }
+
+        // Filter
+        if (category) {
+            query.category = category
+        }
+
+        // Filter by budget
+        if (minBudget && maxBudget) {
+            query.budget = { $gte: minBudget, $lte: maxBudget }
+        }
+
+        // Pagination
+        const pageNumber = parseInt(page) || 1
+        const pageSize = parseInt(limit) || 10
+        const skip = (pageNumber - 1) * pageSize
+
+        // Fetch jobs with filters & pagination
+        const jobs = await Job.find(query)
+            .populate("postedBy", "name email")
+            .skip(skip)
+            .limit(pageSize)
+            .sort({ createdAt: -1 })  // Sort lates jobs
+
+        const totalJobs = await Job.countDocuments(query)
+
+        if (jobs.length === 0) {
+            return res.status(404).json({ message: "No jobs found matching the filters" })
+        }
+
+        res.status(200).json({
+            totalJobs,
+            page: pageNumber,
+            totalPages: Math.ceil(totalJobs / pageSize),
+            jobs
+        })
+
+
     } catch (error) {
         res.status(500).json({ message: "Server Error!", error: error.message })
     }
